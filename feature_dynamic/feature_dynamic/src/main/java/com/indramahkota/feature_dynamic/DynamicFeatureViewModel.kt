a6dafcf6a9b8119c7017e8e5296f2627f0b8ecd8
Indra Mahkota <indramahkota1@gmail.com>
2022-06-04 15:19:19 +0700

    Modify project structure

--- feature_dynamic/feature_dynamic/src/main/java/com/indramahkota/feature_dynamic/DynamicFeatureViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.indramahkota.feature_dynamic
-
-import androidx.lifecycle.ViewModel
-import com.indramahkota.domain.SampleRepository
-import javax.inject.Inject
-
-class DynamicFeatureViewModel @Inject constructor(private val repository: SampleRepository) : ViewModel() {
-    fun getMessage() = repository.getMessage()
-}
